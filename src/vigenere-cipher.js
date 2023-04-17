c3745ca10a9fe83430c76c3aa802e77f13f4781b
sergpet93 <maknev96@mail.ru>
2023-04-17 12:17:32 +0300

    feat: update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor(arg = true){
+    this.arg = arg;
+    this.alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+  encrypt(str, key) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(!str || !key)
+      throw new NotImplementedError('Incorrect arguments!');
+    let result = '';
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let number_str = 0;
+    let number_key = 0;
+    let number = 0;
+    for(let i = 0; i < str.length; i++){
+      if(this.alp.indexOf(str[i]) === -1){
+        result += str[i];
+        number += 1; 
+      }
+      else{
+        number_str = this.alp.indexOf(str[i]);
+        number_key = this.alp.indexOf(key[(i - number) % key.length]) ;
+        result += this.alp[(number_key + number_str) % 26];
+      }
+    }
+    return (this.arg) ? result : result.split('').reverse().join('')
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(str, key) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(!str || !key)
+      throw new NotImplementedError('Incorrect arguments!');
+    let result = '';
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let number_str = 0;
+    let number_key = 0;
+    let number = 0;
+    for(let i = 0; i < str.length; i++){
+      if(this.alp.indexOf(str[i]) === -1){
+        result += str[i];
+        number += 1; 
+      }
+      else{
+        number_str = this.alp.indexOf(str[i]);
+        number_key = this.alp.indexOf(key[(i - number) % key.length]) ;
+        result += this.alp[(26 + number_str - number_key) % 26];
+      }
+    }
+    return (this.arg) ? result : result.split('').reverse().join('')
   }
 }
 
